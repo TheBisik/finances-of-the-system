09e40044ec2b8c4fe77ec3f8e30dfe304d883cb8
Fabian <bisik@vp.pl>
2025-07-31 18:23:31 +0200

    delete temponary files

--- src/main/java/pl/pcdevs/systemfinansowy/controller/Dashboard.java
+++ src/main/java/pl/pcdevs/systemfinansowy/controller/Dashboard.java
@@ -1,14 +1,14 @@
-//package pl.pcdevs.systemfinansowy.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class Dashboard {
-//
-//    @GetMapping("/auth/dashboard")
-//    public String dashboard() {
-//        return "/auth/dashboard";
-//    }
-//
-//}
+package pl.pcdevs.systemfinansowy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Dashboard {
+
+    @GetMapping("/auth/dashboard")
+    public String dashboard() {
+        return "/auth/dashboard";
+    }
+
+}
